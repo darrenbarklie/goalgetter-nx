686b263a59a662a08b44e0bf6221fa2df2a3888d
Darren Barklie <darrenbarklie@gmail.com>
2023-02-11 14:44:41 +0000

    Update README, frontend example fetch

--- packages/goalgetter-web/src/app/app.tsx
+++ packages/goalgetter-web/src/app/app.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
+
 export function App() {
+  const [greeting, setGreeting] = useState<{ message: string }>({
+    message: '',
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/')
+      .then((response) => response.json())
+      .then((data) => {
+        setGreeting(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div>
-      {import.meta.env.VITE_APP_TITLE}
-      <div />
+      <h1>{import.meta.env.VITE_APP_TITLE}</h1>
+      <h2>{greeting && greeting.message}</h2>
     </div>
   );
 }
